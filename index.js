3c87b663322efa27f1e19059d2317e8dc757f1fe
alhanampi <alhanampi@gmail.com>
2021-03-22 21:22:48 -0300

    added per function functionality

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ append()
 
 //nota: correrlo en consola con args si se quiere hacer append:
 // node index --user='{"user":"pam","category":4,"address":"Cabildo 26"}'
+
+
+//para correr las funciones:
+//npm i -g run-func
+//aggregar al package.json:
+//    "append":  "run-func crud.js append"
